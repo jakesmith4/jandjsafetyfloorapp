bb5505d06fdc20f2475696b24ac75afb34a03981
Jacob Smith <jakesmith.seo@gmail.com>
2025-01-14 16:01:15 -0600

    split off AddJob into a new component getting ready to put to modal

---
+++ src/components/AddJob.jsx
@@ -0,0 +1,78 @@
+import { useState } from 'react';
+import { useGlobalContext } from '../context';
+
+const AddJob = () => {
+  const { addJobToCurrentOwner } = useGlobalContext();
+
+  const [date, setDate] = useState('');
+  const [storeNumber, setStoreNumber] = useState('');
+  const [address, setAddress] = useState('');
+  const [price, setPrice] = useState('');
+
+  return (
+    <form
+      className="form"
+      onSubmit={e => {
+        if (!storeNumber || !address || !price || !date) {
+          e.preventDefault();
+          return;
+        }
+        addJobToCurrentOwner(e, storeNumber, address, price, date);
+        setStoreNumber('');
+        setAddress('');
+        setPrice('');
+      }}
+    >
+      <h3>add job</h3>
+      <div className="form-row">
+        <label htmlFor="date" className="form-label">
+          date
+        </label>
+        <input
+          type="date"
+          id="date"
+          className="form-input"
+          value={date}
+          onChange={e => setDate(e.target.value)}
+        />
+      </div>
+      <div className="form-row">
+        <label htmlFor="store-number" className="form-label">
+          store number
+        </label>
+        <input
+          type="text"
+          id="store-number"
+          className="form-input"
+          value={storeNumber}
+          onChange={e => setStoreNumber(e.target.value)}
+        />
+      </div>
+      <div className="form-row">
+        <label htmlFor="address" className="form-label">
+          address
+        </label>
+        <textarea
+          id="address"
+          className="form-textarea"
+          value={address}
+          onChange={e => setAddress(e.target.value)}
+        ></textarea>
+      </div>
+      <div className="form-row">
+        <label htmlFor="price" className="form-label">
+          price
+        </label>
+        <input
+          type="number"
+          id="price"
+          className="form-input"
+          value={price}
+          onChange={e => setPrice(e.target.value)}
+        />
+      </div>
+      <button className="btn">add job</button>
+    </form>
+  );
+};
+export default AddJob;
--- src/components/CurrentOwner.jsx
+++ src/components/CurrentOwner.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 import { useGlobalContext } from '../context';
+import AddJob from './AddJob';
 
 const CurrentOwner = () => {
   const {
     currentOwner,
     editOwner,
-    addJobToCurrentOwner,
     toggleCurrentOwnerForm,
     isCurrentOwnerFormOpen,
     openCurrentJob,
@@ -14,10 +14,7 @@ const CurrentOwner = () => {
   const [name, setName] = useState(currentOwner?.name);
   const [numberOfStores, setNumberOfStores] = useState(currentOwner?.amount);
   const [ownerPrice, setOwnerPrice] = useState(currentOwner?.price);
-  const [date, setDate] = useState('');
-  const [storeNumber, setStoreNumber] = useState('');
-  const [address, setAddress] = useState('');
-  const [price, setPrice] = useState('');
+
   const [showCompletedJobs, setShowCompletedJobs] = useState(false);
 
   const jobsArray = currentOwner ? currentOwner.jobs : [];
@@ -118,71 +115,7 @@ const CurrentOwner = () => {
         {isCurrentOwnerFormOpen ? 'close form' : 'add job'}
       </button>
 
-      {isCurrentOwnerFormOpen && (
-        <form
-          className="form"
-          onSubmit={e => {
-            if (!storeNumber || !address || !price || !date) {
-              e.preventDefault();
-              return;
-            }
-            addJobToCurrentOwner(e, storeNumber, address, price, date);
-            setStoreNumber('');
-            setAddress('');
-            setPrice('');
-          }}
-        >
-          <h3>add job</h3>
-          <div className="form-row">
-            <label htmlFor="date" className="form-label">
-              date
-            </label>
-            <input
-              type="date"
-              id="date"
-              className="form-input"
-              value={date}
-              onChange={e => setDate(e.target.value)}
-            />
-          </div>
-          <div className="form-row">
-            <label htmlFor="store-number" className="form-label">
-              store number
-            </label>
-            <input
-              type="text"
-              id="store-number"
-              className="form-input"
-              value={storeNumber}
-              onChange={e => setStoreNumber(e.target.value)}
-            />
-          </div>
-          <div className="form-row">
-            <label htmlFor="address" className="form-label">
-              address
-            </label>
-            <textarea
-              id="address"
-              className="form-textarea"
-              value={address}
-              onChange={e => setAddress(e.target.value)}
-            ></textarea>
-          </div>
-          <div className="form-row">
-            <label htmlFor="price" className="form-label">
-              price
-            </label>
-            <input
-              type="number"
-              id="price"
-              className="form-input"
-              value={price}
-              onChange={e => setPrice(e.target.value)}
-            />
-          </div>
-          <button className="btn">add job</button>
-        </form>
-      )}
+      {isCurrentOwnerFormOpen && <AddJob />}
 
       <h2>Jobs</h2>
 
@@ -219,7 +152,7 @@ const CurrentOwner = () => {
 
               <h3>date:</h3>
               <span className="jobs-item-info">
-                {Intl.DateTimeFormat('en-US').format(new Date(job.dateObject))}
+                {Intl.DateTimeFormat('en-US').format(new Date(job.date))}
               </span>
 
               <input type="date" defaultValue={job.date} />
