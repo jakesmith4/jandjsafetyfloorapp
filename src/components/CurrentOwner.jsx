dae727969b8e42bd81bfa591d7c84caeb38c4aa3
Jacob Smith <jakesmith.seo@gmail.com>
2025-01-21 02:08:34 -0600

    added in lifetime amount

--- src/components/CurrentOwner.jsx
+++ src/components/CurrentOwner.jsx
@@ -29,6 +29,10 @@ const CurrentOwner = () => {
     job => job.completed === false
   );
 
+  const lifeTimeAmount = currentOwner.jobs
+    .map(job => +job.price)
+    .reduce((accumulator, current) => accumulator + current, 0);
+
   let currentJobs;
 
   showCompletedJobs === false
@@ -196,18 +200,30 @@ const CurrentOwner = () => {
       <footer>
         <hr />
         <div>
-          <h5 className="owner-total">
-            <span className="owner-total-heading">total amount:</span>{' '}
-            <span className="owner-total-value">${thisTimesPrice}</span>
-          </h5>
-          <h5 className="owner-total">
-            <span className="owner-total-heading">jake's cut:</span>{' '}
+          <div className="owner-total">
+            <span className="owner-total-heading">uncompleted jobs:</span>
+            <span className="owner-total-value">{unCompletedJobs.length}</span>
+          </div>
+          <div className="owner-total">
+            <span className="owner-total-heading">completed jobs:</span>
+            <span className="owner-total-value">{completedJobs.length}</span>
+          </div>
+          <div className="owner-total">
+            <span className="owner-total-heading">jake's cut:</span>
             <span className="owner-total-value">${jakesCut}</span>
-          </h5>
-          <h5 className="owner-total">
-            <span className="owner-total-heading">john's cut:</span>{' '}
+          </div>
+          <div className="owner-total">
+            <span className="owner-total-heading">john's cut:</span>
             <span className="owner-total-value">${johnsCut}</span>
-          </h5>
+          </div>
+          <div className="owner-total">
+            <span className="owner-total-heading">total amount:</span>
+            <span className="owner-total-value">${thisTimesPrice}</span>
+          </div>
+          <div className="owner-total">
+            <span className="owner-total-heading">lifetime amount:</span>
+            <span className="owner-total-value">${lifeTimeAmount}</span>
+          </div>
         </div>
       </footer>
     </section>
