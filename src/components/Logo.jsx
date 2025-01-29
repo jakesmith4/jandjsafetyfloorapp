db47adca8a8cdbc99c4b948eefdbd4ae8a7f3c76
Jacob Smith <jakesmith.seo@gmail.com>
2025-01-28 19:32:23 -0600

    added css for search jobs

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,7 @@
 const Logo = ({ logo }) => {
   return (
     <div className="owner-info-heading">
-      <h2>J&J Safety Floor</h2>
+      <h1>J&J Safety Floor</h1>
       <img
         src={logo}
         alt="J and J Safety Floor Logo"
--- src/components/SearchJobs.jsx
+++ src/components/SearchJobs.jsx
@@ -7,18 +7,28 @@ const SearchJobs = () => {
   jobsFound?.sort((a, b) => new Date(a.date) - new Date(b.date));
 
   return (
-    <section className="completed-jobs current-owner">
+    <section className="search-jobs current-owner">
+      <h2 className="search-jobs-heading">search jobs</h2>
       <input
         type="text"
         id="search"
+        className="form-input search-input"
         value={searchInputValue}
         onChange={e => {
           searchJob(e.target.value);
         }}
       />
-      <h2>
-        {jobsFound?.length} jobs found for "{searchInputValue}"
-      </h2>
+      <h3 className="jobs-found-heading">
+        <span className="jobs-found-text">
+          <span className="jobs-found-amount">{jobsFound?.length}</span> jobs
+          found for
+        </span>
+        <span className="jobs-found-value">
+          <span className="jobs-found-quote">"</span>
+          <span>{searchInputValue}</span>
+          <span className="jobs-found-quote">"</span>
+        </span>
+      </h3>
       {jobsFound?.length > 0
         ? jobsFound.map(job => <SingleJob job={job} key={job.id} />)
         : null}
